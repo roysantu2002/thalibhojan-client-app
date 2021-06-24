b17620c72c4b8824a9262b7f5e90e5378d16fb7e
“Santu <roysantu2002@yahoo.com>
2021-06-24 15:48:01 +0530

    added fixed tonext config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
+require("dotenv").config();
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+  distDir: "build",
+
+  experimental: {
+    optimizeFonts: true,
+    optimizeImages: true,
+  },
+
+  generateEtags: false,
+  // Target must be serverless
+  target: "serverless",
+
+  webpack: (config) => {
+    // config.resolve.modules.push(path.resolve("./"));
+    config.plugins = config.plugins || [];
+    config.plugins = [
+      ...config.plugins,
+
+      new Dotenv({
+        path: path.join(__dirname, ".env.local"),
+        systemvars: true,
+      }),
+    ];
     return config;
   },
-};
+});
